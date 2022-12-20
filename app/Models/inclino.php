b36398a04a92eb01ebe2fa0c7adcecf206e3aa5e
Franco <frank.13.gp@gmail.com>
2022-12-19 19:57:13 -0500

    Referenciar tablas y vistas

---
+++ app/Models/inclino.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class inclino extends Model
+{
+    use HasFactory;
+
+    protected $table = 'inclino';
+    /* public function getCountAttribute()
+    {
+        return $this->count();
+    } */
+}
