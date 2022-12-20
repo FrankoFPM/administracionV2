7daf63423f5df675c85b108fb1875a53b2855a54
Franco <frank.13.gp@gmail.com>
2022-12-19 19:57:30 -0500

    Devolver variables a index/dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\inclino;
+use App\Models\vdeudageneral;
+use App\Models\vtotalmora;
+use App\Models\vusuariosdeuda;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('admin.index');
+        $count = inclino::count();
+        $totalD = vdeudageneral::first()->total;
+        $usuariosD = vusuariosdeuda::count();
+        $mora = vtotalmora::first()->total_mora;
+        return view('admin.index')->with(['count'=> $count,'total'=> $totalD, 'usuarios'=> $usuariosD, 'mora'=> $mora]);
     }
 }
