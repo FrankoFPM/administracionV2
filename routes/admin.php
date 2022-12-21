8a38845c97a739e0adffe3715f530afa061db85a
Franco <frank.13.gp@gmail.com>
2022-12-20 19:49:02 -0500

    Grupo de rutas

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\Usuario\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('', [HomeController::class, 'index']);
+
+Route::controller(UsuarioController::class)->group(function(){
+    Route::get('/usuarios','index');
+    Route::get('/usuarios/add','create');
+    Route::get('/usuarios/edit','edit');
+});
