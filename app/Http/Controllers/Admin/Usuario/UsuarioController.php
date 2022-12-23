2fe2de17e667a9edd7fd14fa7538ff23ac66895b
Franco <frank.13.gp@gmail.com>
2022-12-22 19:19:20 -0500

    Mostrar registros con datatables

--- app/Http/Controllers/Admin/Usuario/UsuarioController.php
+++ app/Http/Controllers/Admin/Usuario/UsuarioController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Admin\Usuario;
 
 use App\Http\Controllers\Controller;
+use App\Models\inclino;
 use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
 {
     public function index(){
-        return view('admin.usuarios.index');
+        $data = inclino::all();
+        return view('admin.usuarios.index', compact('data'));
     }
     public function create(){
         return view('admin.usuarios.add');
