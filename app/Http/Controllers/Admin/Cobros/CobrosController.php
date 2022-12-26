9bc9ccf60a63af9e3425e7d2b555ef4704ea8a87
Franco <frank.13.gp@gmail.com>
2022-12-25 19:04:29 -0500

    Crear registro de mora

--- app/Http/Controllers/Admin/Cobros/CobrosController.php
+++ app/Http/Controllers/Admin/Cobros/CobrosController.php
@@ -5,22 +5,26 @@ namespace App\Http\Controllers\Admin\Cobros;
 use App\Http\Controllers\Controller;
 use App\Models\cobro;
 use App\Models\inclino;
+use App\Models\mora;
 use App\Models\vinclinosincobro;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class CobrosController extends Controller
 {
-    public function index(){
-        $data = cobro::join('inclino', 'cobro.id_arrendatario','=','inclino.id')->select('inclino.*', 'cobro.*')->get();
+    public function index()
+    {
+        $data = cobro::join('inclino', 'cobro.id_arrendatario', '=', 'inclino.id')->select('inclino.*', 'cobro.*')->get();
         $count = cobro::count();
         return view('admin.cobros.index', compact('data', 'count'));
     }
-    public function create(){
+    public function create()
+    {
         $usuario = vinclinosincobro::all();
         return view('admin.cobros.add', compact('usuario'));
     }
-    public function search(inclino $inclino){
+    public function search(inclino $inclino)
+    {
         return $inclino;
     }
     public function register(Request $request)
@@ -34,17 +38,25 @@ class CobrosController extends Controller
         $cobro->id_arrendatario = $request->input('arrendatario');
         $cobro->save();
 
+        $mora = new mora;
+        $mora->total_mora = 0;
+        $mora->id_usuario  = $request->input('arrendatario');
+        $mora->save();
+
         return redirect('admin/cobros')->with('SUCCESS', true);
     }
-    public function edit(cobro $cobro){
-        $data = cobro::join('inclino', 'cobro.id_arrendatario','=','inclino.id')->select('inclino.*', 'cobro.*')->where('cobro.id',$cobro->id)->first();
+    public function edit(cobro $cobro)
+    {
+        $data = cobro::join('inclino', 'cobro.id_arrendatario', '=', 'inclino.id')->select('inclino.*', 'cobro.*')->where('cobro.id', $cobro->id)->first();
         return view('admin/cobros/edit', compact('data'));
         //return $data;
     }
-    public function delete(cobro $cobro){
+    public function delete(cobro $cobro)
+    {
         $cobro->delete();
     }
-    public function update(Request $request, cobro $cobro){
+    public function update(Request $request, cobro $cobro)
+    {
         $cobro->deuda_inmueble = $request->input('deuda');
         $cobro->serv_luz = $request->input('luz');
         $cobro->serv_agua = $request->input('agua');
